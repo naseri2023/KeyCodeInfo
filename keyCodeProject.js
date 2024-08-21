0a8aea5a70aa0c7333d8e031056beeabab2e4010
Zahra Naseri <139186492+naseri2023@users.noreply.github.com>
2024-08-21 23:17:40 +0330

    Add files via upload

---
+++ keyCodeProject.js
@@ -0,0 +1,17 @@
+let onKeyDown = document.getElementById("bodyClass")
+let key = document.getElementById("key")
+let keyCode = document.getElementById("keyCode")
+let code = document.getElementById("code")
+let locationKey = document.getElementById("location")
+
+
+onKeyDown.addEventListener("keydown",function (event) {
+    event.preventDefault()
+    console.log(event)
+    key.innerHTML = event.key
+    keyCode.innerHTML = event.keyCode
+    code.innerHTML = event.code
+    locationKey.innerHTML = event.location
+})
+
+
